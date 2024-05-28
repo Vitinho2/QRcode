d66272739b4ef14337824318aeed50a1ef4ed164
Vitinho2 <vi.vitorsouza150@gmail.com>
2024-05-28 13:46:25 -0300

    Projeto QRcode

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+export function App() {
+
+  return (
+    <>
+      <main>
+        <div className="cardQRcode">
+          <div className="qrcode"></div>
+          <div className="description">
+            <p className="textPrinci">
+              Improve your front-end <br />
+              skills by building projects
+            </p>
+            <p className="textSecondary">
+              Scan the QR code to visit Frontend <br />
+              Mentor and take your coding skills to <br />
+              the next level
+            </p>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
+
